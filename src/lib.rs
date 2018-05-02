91be10708d1d4d3fd88ae95e48f144256e0dfe88
Nick Price <nick@spun.io>
2018-05-02 10:44:43 -0400

    use more consts from libc crate

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,9 @@ use std::mem;
 
 use libc::{bind, c_char, c_int, c_short, c_ulong, c_void, getpid, getsockopt, if_nametoindex,
            ioctl, mmap, poll, pollfd, setsockopt, sockaddr, sockaddr_ll, socket, socklen_t,
-           AF_PACKET, MAP_LOCKED, MAP_NORESERVE, MAP_SHARED, PF_PACKET, POLLERR, POLLIN,
-           PROT_READ, PROT_WRITE, SOCK_RAW, SOL_PACKET, SOL_SOCKET};
-
-const ETH_P_ALL: u16 = 3;
-const ETH_P_IP: u16 = 2048; //0x0800;
-const ETH_ALEN: c_int = 6;
+           AF_PACKET, ETH_ALEN, ETH_P_ALL, ETH_P_IP, IFF_PROMISC, MAP_LOCKED, MAP_NORESERVE,
+           MAP_SHARED, PF_PACKET, POLLERR, POLLIN, PROT_READ, PROT_WRITE, SOCK_RAW, SOL_PACKET,
+           SOL_SOCKET};
 
 //const PACKET_STATISTICS: c_int = 6;
 const PACKET_RX_RING: c_int = 5;
@@ -37,8 +34,6 @@ const TPACKET_V3: c_int = 2;
 
 const SIOCGIFFLAGS: c_ulong = 35091; //0x00008913;
 const SIOCSIFFLAGS: c_ulong = 35092; //0x00008914;
-
-const IFF_PROMISC: c_ulong = 256; //1 << 8;
 
 const IFNAMESIZE: usize = 16;
 const IFREQUNIONSIZE: usize = 24;
@@ -287,7 +282,7 @@ impl Ring {
     }
 
     pub fn set_promisc(&mut self) -> io::Result<()> {
-        self.set_flag(IFF_PROMISC)
+        self.set_flag(IFF_PROMISC as u64)
     }
 
     fn ioctl(&self, ident: c_ulong, if_req: IfReq) -> io::Result<IfReq> {
