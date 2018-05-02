f1c56d4f3474c2e0659fd19e4637d53bfeeb41d8
Nick Price <nick@spun.io>
2018-05-02 12:53:20 -0400

    not bad

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -7,18 +7,27 @@ use std::thread;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    for _ in 0..num_cpus::get() {
+    for x in 0..num_cpus::get() {
         let interface = args[1].clone();
         thread::spawn(move || {
+            let tid = x;
+            let mut packets: u64 = 0;
+            let mut drops: u64 = 0;
             let ring = af_packet::Ring::from_if_name(&interface).unwrap();
             loop {
                 let mut block = ring.get_block(); //THIS WILL BLOCK
-                for packet in block.get_raw_packets() {
-                    println!("{:?}", packet);
+                for _packet in block.get_raw_packets() {
+                    //do something
+                    //println!("{:?}", packet);
                 }
 
                 //these stats are updated per-block and are not cumulative
-                println!("{:?}", ring.get_rx_statistics());
+                let stats = ring.get_rx_statistics().unwrap();
+
+                packets += stats.tp_packets as u64;
+                drops += stats.tp_drops as u64;
+
+                println!("Thread {} has received {} packets and dropped {}", tid, packets, drops);
 
                 block.mark_as_consumed();
             }
